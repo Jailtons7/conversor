323216554163305f96cb9f4fcbc9ff9d7fd6758e
Jailton Silva <jss1@ifal.edu.br>
2021-08-31 17:46:23 -0300

    feat: addin api view to handle requests

---
+++ api/views.py
@@ -0,0 +1,26 @@
+from rest_framework import views
+from rest_framework.response import Response
+from rest_framework import status
+
+from integrations.awesomeapi import AwesomeApi
+
+
+class ConvertView(views.APIView):
+    def get(self, request):
+        """
+        method GET
+        """
+        params_data = (
+            request.GET.get('from'),
+            request.GET.get('to'),
+            request.GET.get('amount'),
+        )
+
+        if not any(params_data):
+            return Response(
+                data={'erro': 'Verifique se os parâmetros foram passados na URL'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        awesome = AwesomeApi(from_currency=params_data[0], to_currency=params_data[1])
+        converted = awesome.converts(params_data[2])
+        return Response(data={'converte': converted}, status=status.HTTP_200_OK)
